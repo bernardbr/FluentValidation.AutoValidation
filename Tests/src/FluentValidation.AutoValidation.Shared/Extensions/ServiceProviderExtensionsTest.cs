7e493a58683f1150c7f54c590cd6427cb1b506c8
Bernardo Esbérard <bernardbr@users.noreply.github.com>
2026-02-28 22:43:33 -0300

    :white_check_mark: Create test for ServiceProviderExtensions.cs

--- Tests/src/FluentValidation.AutoValidation.Shared/Extensions/ServiceProviderExtensionsTest.cs
+++ Tests/src/FluentValidation.AutoValidation.Shared/Extensions/ServiceProviderExtensionsTest.cs
@@ -4,6 +4,7 @@ using System;
 using FluentValidation;
 using NSubstitute;
 using SharpGrip.FluentValidation.AutoValidation.Shared.Extensions;
+using SharpGrip.FluentValidation.AutoValidation.Tests.Shared.Stubs;
 using Xunit;
 
 namespace SharpGrip.FluentValidation.AutoValidation.Tests.FluentValidation.AutoValidation.Shared.Extensions;
@@ -20,8 +21,26 @@ public class ServiceProviderExtensionsTest
 
         serviceProvider.GetService(typeof(IValidator<>).MakeGenericType(testModel.GetType())).Returns(testModelValidator);
 
-        var validator = serviceProvider.GetValidator(testModel.GetType());
+        var validator = serviceProvider.GetValidator(testModel.GetType(), false);
 
+        Assert.Equal(testModelValidator, validator);
+    }
+
+    [Fact]
+    public void Test_GetValidator_WithBaseTypeValidator()
+    {
+        var serviceProvider = Substitute.For<IServiceProvider>();
+
+        var testModel = new CreatePostRequest();
+        var testModelValidator = new CreatePostValidator();
+
+        serviceProvider.GetService(typeof(IValidator<>).MakeGenericType(testModel.GetType())).Returns(null);
+        serviceProvider.GetService(typeof(IValidator<>).MakeGenericType(typeof(ICreatePost))).Returns(testModelValidator);
+
+        var validator = serviceProvider.GetValidator(testModel.GetType(), false);
+        Assert.Null(validator);
+
+        validator = serviceProvider.GetValidator(testModel.GetType(), true);
         Assert.Equal(testModelValidator, validator);
     }
 
---
+++ Tests/src/Shared/Stubs/CreatePostRequest.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace SharpGrip.FluentValidation.AutoValidation.Tests.Shared.Stubs;
+
+public interface ICreatePost
+{
+    string Body { get; }
+    string Title { get; }
+}
+
+public class CreatePostRequest : ICreatePost
+{
+    public string Body { get; set; } = null!;
+    public string Title { get; set; } = null!;
+}
+
+public class CreatePostValidator : AbstractValidator<ICreatePost>
+{
+    public CreatePostValidator()
+    {
+        RuleFor(req => req.Title).NotEmpty().WithMessage("Title cannot be null or empty.");
+        RuleFor(req => req.Body).NotEmpty().WithMessage("Body cannot be null or empty.");
+    }
+}
