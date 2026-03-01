ece88cb1bef9e6f940af3df0e836ec710c9c3212
Bernardo Esbérard <bernardbr@users.noreply.github.com>
2026-02-28 22:52:28 -0300

    :white_check_mark: add global culture setup for tests to prevent localized dotnet failures

---
+++ Tests/src/Shared/GlobalSetup.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using System.Threading;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+[assembly: Xunit.TestFramework("SharpGrip.FluentValidation.AutoValidation.Tests.Shared.GlobalSetup", "FluentValidation.AutoValidation.Tests")]
+namespace SharpGrip.FluentValidation.AutoValidation.Tests.Shared;
+
+public class GlobalSetup : XunitTestFramework
+{
+    public GlobalSetup(IMessageSink messageSink) : base(messageSink)
+    {
+        SetupEnvironment();
+    }
+
+    private static void SetupEnvironment()
+    {
+        Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-US");
+    }
+}
