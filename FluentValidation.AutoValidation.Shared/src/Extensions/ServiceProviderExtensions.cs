e0ef77e4ccbede30d65858c462750d454cf580d5
Bernardo Esbérard <bernardbr@users.noreply.github.com>
2026-02-28 22:37:54 -0300

    :sparkles: Change the GetValidator method to respect the configuration.

--- FluentValidation.AutoValidation.Shared/src/Extensions/ServiceProviderExtensions.cs
+++ FluentValidation.AutoValidation.Shared/src/Extensions/ServiceProviderExtensions.cs
@@ -1,13 +1,46 @@
 ﻿using System;
 using FluentValidation;
 
-namespace SharpGrip.FluentValidation.AutoValidation.Shared.Extensions
+namespace SharpGrip.FluentValidation.AutoValidation.Shared.Extensions;
+
+public static class ServiceProviderExtensions
 {
-    public static class ServiceProviderExtensions
+    public static object? GetValidator(this IServiceProvider serviceProvider, Type type, bool useBaseTypeValidations)
     {
-        public static object? GetValidator(this IServiceProvider serviceProvider, Type type)
+        var validator = serviceProvider.GetService(typeof(IValidator<>).MakeGenericType(type));
+        if (validator is not null) return validator;
+        if (!useBaseTypeValidations) return null;
+
+        return GetValidatorFromBaseClasses(serviceProvider, type)
+               ?? GetValidatorFromInterfaces(serviceProvider, type);
+    }
+
+    private static object? GetValidatorFromBaseClasses(IServiceProvider serviceProvider, Type type)
+    {
+        var baseType = type.BaseType;
+        while (baseType is not null && baseType != typeof(object))
         {
-            return serviceProvider.GetService(typeof(IValidator<>).MakeGenericType(type));
+            var baseValidatorType = typeof(IValidator<>).MakeGenericType(baseType);
+            var baseValidator = serviceProvider.GetService(baseValidatorType);
+
+            if (baseValidator is not null) return baseValidator;
+
+            baseType = baseType.BaseType;
         }
+
+        return null;
+    }
+
+    private static object? GetValidatorFromInterfaces(IServiceProvider serviceProvider, Type type)
+    {
+        foreach (var interfaceType in type.GetInterfaces())
+        {
+            var interfaceValidatorType = typeof(IValidator<>).MakeGenericType(interfaceType);
+            var interfaceValidator = serviceProvider.GetService(interfaceValidatorType);
+
+            if (interfaceValidator is not null) return interfaceValidator;
+        }
+
+        return null;
     }
 }
